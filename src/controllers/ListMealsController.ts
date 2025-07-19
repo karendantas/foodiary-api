2949a0b3f81253930bee172d5ee299931d0ff593
karendantas <karennobre74@gmail.com>
2025-07-19 16:59:38 -0300

    feat: create listing meals route

---
+++ src/controllers/ListMealsController.ts
@@ -0,0 +1,41 @@
+import { and, eq, gte, lte } from "drizzle-orm";
+import { db } from "../db";
+import { HttpResponse, ProtectedHttpRequest } from "../types/http";
+import { badRequest, ok } from "../utils/http";
+
+import z from 'zod'
+import { mealsTable } from "../db/schema";
+
+const schema = z.object({
+    date: z.iso.date().transform(dateStr => new Date(dateStr))
+})
+export class ListMealsController {
+    static async handle({userId, queryParams}: ProtectedHttpRequest): Promise<HttpResponse>{
+        console.log('USER', userId)
+        const {success, error, data} = schema.safeParse(queryParams)
+
+        if (!success){
+            return badRequest({errors: error.issues})
+        }
+
+        const endDate = new Date (data.date)
+        endDate.setUTCHours(23,59,59,59)
+        const meal = await db.query.mealsTable.findMany({
+            columns: {
+                id: true,
+                foods: true,
+                createdAt: true,
+                icon: true,
+                name: true,
+            },
+            where: and( 
+                eq(mealsTable.userId, userId),
+                eq(mealsTable.status, 'success'),
+                gte(mealsTable.createdAt, data.date),
+                lte(mealsTable.createdAt, endDate),
+            ),
+        })
+       
+        return ok({meal})
+    }
+}
