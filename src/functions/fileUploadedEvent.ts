e5850a9e696648440e77220ada6f665e23f83037
karendantas <karennobre74@gmail.com>
2025-07-19 19:08:00 -0300

    feat: publish on queue

--- src/functions/fileUploadedEvent.ts
+++ src/functions/fileUploadedEvent.ts
@@ -1,7 +1,15 @@
+import { SendMessageCommand } from "@aws-sdk/client-sqs";
 import { S3Event } from "aws-lambda";
+import { sqsClient } from "../clients/sqsClient";
 
 export async function handler(event: S3Event){
-    await Promise.all(event.Records.map(record => {
-        record.s3.object.key
-    })) //chama a lambda quando tiver a key de um arquivo upado
+    await Promise.all(event.Records.map(async record => {
+        const command = new SendMessageCommand({
+            QueueUrl: 'meals-queue',
+            MessageBody: JSON.stringify({fileKey: record.s3.object.key})
+        })
+
+        await sqsClient.send(command)
+      
+    })) //chama a lambda quando tiver a key de um arquivo upado e envia pra fila
 }
